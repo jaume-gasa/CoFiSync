a7f2681dd61f77c5bb37678335e5f154e0e042f1
Unai Pastor Garrido <unaipg@gmail.com>
2018-07-15 00:18:16 +0200

    Added a config file to change the max size of the line in pycodestyle

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,11 +1,11 @@
-"""Test just to check travis integration when real code appears this should be deleted"""
+"""Test just to check travis integration when real code appears this should be deleted."""
 from unittest import TestCase
 
 
 class BaseTest(TestCase):
-    """Test to check everything is ok with travis integration"""
+    """Test to check everything is ok with travis integration."""
 
     def test_maths(self):
-        """Check maths still work at the most basic level"""
+        """Check maths still work at the most basic level."""
 
         self.assertEqual(2, 1 + 1)
