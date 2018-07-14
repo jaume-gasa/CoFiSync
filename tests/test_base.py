a16d585390f81f12c3141188cd58dff4c9160009
Unai Pastor Garrido <unaipg@gmail.com>
2018-07-14 23:49:13 +0200

    Added travis support and a base test
    The current test is just a placeholder to check travis is running the tests, style checks and docs checks

---
+++ tests/test_base.py
@@ -0,0 +1,11 @@
+"""Test just to check travis integration when real code appears this should be deleted"""
+from unittest import TestCase
+
+
+class BaseTest(TestCase):
+    """Test to check everything is ok with travis integration"""
+
+    def test_maths(self):
+        """Check maths still work at the most basic level"""
+
+        self.assertEqual(2, 1 + 1)
